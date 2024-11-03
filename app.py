d32936447e2e94dd100e0c1a4e2a40770c9c2c7b
tenisaname <andreeivanovich@gmail.com>
2024-11-03 21:37:45 +0500

    lesson_three

--- app.py
+++ app.py
@@ -36,4 +36,16 @@ def index():
 def user(name):
     return render_template("user.html", user_name=name)
 
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"),500
+
 app.run(debug=True)
